78572d7eed4d61082c707516c2fc17a9f1a5b187
KinnarChowdhury1994 <kc1988@cse.jgec.ac.in>
2024-03-28 18:26:50 +0530

    modified python code

--- automate.py
+++ automate.py
@@ -34,7 +34,14 @@ class Automation:
         try:
             if self.datetimeUtil():
                 self.ManageCredentials()
-                self.retrieve_latest_commit_date_for_github_repository(in_username=str(self.owner),in_repository_name=str(REPOSITORY))
+                gitResp = self.retrieve_latest_commit_date_for_github_repository(in_repository_name=str(REPOSITORY))
+                self.log.warning(gitResp)
+                
+                if gitResp:
+                    self.log.warning('Processing for Deployment')
+                else:
+                    self.log.warning('No Deployment')
+                
             else:
                 raise Exception('Error Occured')
         except Exception as e:
@@ -68,7 +75,7 @@ class Automation:
         self.access_token = str(os.getenv('ACCESS_TOKEN'))
         self.headers = {'Authorization':"Bearer " + self.access_token}
 
-    def retrieve_latest_commit_date_for_github_repository(self,in_username, in_repository_name):
+    def retrieve_latest_commit_date_for_github_repository(self, in_repository_name):
         """Retrieves the date of the last commit for the master branch of the user's GitHub repository.
 
         :param in_username: Name of user which repository to get last commit for.
@@ -76,19 +83,20 @@ class Automation:
         :return: String containing date of last commit, or None if GitHub request failed.
         """
         https_conn = http.client.HTTPSConnection(f'{GITHUB_API}')
-        repository_last_commit_date = None
+        repo_last_commit_date = None
         http_request_headers = {"Accept": "application/vnd.github.v3+json", "User-Agent": self.owner}
-
+        latest_push = False
         try:
             # Request only the one last commit for the supplied user's repository with supplied name.
             self.log.info('Inside retrieve_latest_commit_date_for_github_repository function')
-            github_request_path = "/repos/" + in_username + "/" + in_repository_name + "/commits?page=1&per_page=1"
+            # github_request_path = f"/repos/{str(self.owner)}/{in_repository_name}/commits?page=1&per_page=1"
+            github_request_path = f"/repos/{str(self.owner)}/{in_repository_name}/commits"
             self.log.warning(f'github_request_path -> {github_request_path}')
             
             https_conn.request(url=github_request_path,method='GET',headers=http_request_headers)
             git_api_resp = https_conn.getresponse()
             resp_data = git_api_resp.read().decode(f'{UTF8}')
-            self.log.warning(f'Status :: {git_api_resp.status} | Github API Response ->\n{resp_data}')
+            self.log.warning(f'Status :: {git_api_resp.status}')    # | Github API Response ->\n{resp_data}')
             
             if git_api_resp.status == 200:
                 # Response was successful, now read and parse the JSON data.
@@ -98,17 +106,48 @@ class Automation:
                 api_resp_object = json.loads(api_resp_text)
                 # self.log.warning(f'api_resp_object\n{api_resp_object}')
                 
-                last_commit_ref = api_resp_object[0]['sha']
-                last_commit_msg = api_resp_object[0]['commit']['message']
-                committed_by = api_resp_object[0]['commit']['committer']['name']
-                repo_last_commit_date = api_resp_object[0]['commit']['author']['date']
-                self.log.warning(f'repository_last_commit_date -> {repository_last_commit_date}')
-                self.log.warning(f'Reference {last_commit_ref} [{last_commit_ref[0:7]}] | Last Commit Message -> {last_commit_msg} | By {committed_by} on {repo_last_commit_date}')
+                interval = 0
+                total_len = len(api_resp_object)
+                self.log.info(f'Total Commits :: {total_len}')
+                
+                curr_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+                curr_time = datetime.strptime(curr_time,"%Y-%m-%dT%H:%M:%SZ")
+                self.log.warning(f'Current Time -> {curr_time}')
+                
+                for i in range(len(api_resp_object)):
+                    last_commit_ref = api_resp_object[i]['sha']
+                    last_commit_msg = api_resp_object[i]['commit']['message']
+                    committed_by = api_resp_object[i]['commit']['committer']['name']
+                    repo_last_commit_date = api_resp_object[i]['commit']['author']['date']
+                    # self.log.warning(f'repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
+                    repo_last_commit_date = datetime.strptime(repo_last_commit_date, "%Y-%m-%dT%H:%M:%SZ")
+                    # self.log.warning(f'[CONVERT] repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
+                    
+                    
+                    interval = (curr_time - repo_last_commit_date)
+                    seconds = interval.total_seconds()
+                    hours = seconds // 3600
+                    minutes = (seconds % 3600) // 60
+                    seconds = seconds % 60
+                    
+                    self.log.info(f'Interval of Current Time & Last Commit Time :: {interval} Timedelta [ H :: {hours} | M :: {minutes} | S :: {seconds}]')
+                    
+                    if ((minutes > 0.0 and minutes < 5.0) and (hours == 0.0)):
+                        self.log.info('New Commit Found')
+                        latest_push = True
+                    else:
+                        self.log.info('No New Commit Found')
+                    self.log.warning(f'Reference {last_commit_ref} [{last_commit_ref[0:7]}] | Last Commit Message -> {last_commit_msg}\n::|:: BY - {committed_by} ON - {repo_last_commit_date}')                    
             else:
                 message = f"ERROR: Request to GitHub failed with status {git_api_resp.status} and the reason was {git_api_resp.reason}"
                 self.log.error(f'Message -> {message}')
+        except Exception as e:
+            self.log.exception(e)
+            latest_push = False
         finally:
             https_conn.close()
+            self.log.warning("HTTP Connection Closed Successfully")
+            return latest_push
 
 def main():
     clsUnit = Automation()
@@ -117,7 +156,7 @@ def main():
     except Exception as e:
         clsUnit.log.exception(e)
     finally:
-        return 200
+        return 200,"SUCCESS"
     
     
 if __name__ == "__main__":
