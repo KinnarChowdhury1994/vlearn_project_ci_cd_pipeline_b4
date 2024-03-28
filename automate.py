b2e6aa0d9fe166fb2f2feb8502bc270933903999
KinnarChowdhury1994 <kc1988@cse.jgec.ac.in>
2024-03-28 18:37:06 +0530

    modified automate.py

--- automate.py
+++ automate.py
@@ -25,6 +25,7 @@ from dotenv import load_dotenv
 from constants import GITHUB_API,REPOSITORY,UTF8
 import http.client
 import json
+import subprocess
 
 class Automation:
     def __init__(self):
@@ -39,6 +40,15 @@ class Automation:
                 
                 if gitResp:
                     self.log.warning('Processing for Deployment')
+                    subprocess.Popen('echo "Testing in Server Console"', shell=True)
+                    subprocess.Popen('pwd', shell=True)
+                    subprocess.Popen('ls -la', shell=True)
+                    subprocess.Popen('cd /home/kinnar/vlearn_project_ci_cd_pipeline_b4"', shell=True)
+                    subprocess.Popen('pwd', shell=True)
+                    subprocess.Popen('ls -l', shell=True)
+                    subprocess.Popen('./_deploy.sh', shell=True)
+                    self.log.warning('Deployment Script Triggered Successfully')
+                    
                 else:
                     self.log.warning('No Deployment')
                 
@@ -81,6 +91,7 @@ class Automation:
         :param in_username: Name of user which repository to get last commit for.
         :param in_repository_name Name of user's repository for which to get last commit for.
         :return: String containing date of last commit, or None if GitHub request failed.
+        :Link: https://www.ivankrizsan.se/2017/03/19/interacting-with-github-using-python/
         """
         https_conn = http.client.HTTPSConnection(f'{GITHUB_API}')
         repo_last_commit_date = None
@@ -89,8 +100,8 @@ class Automation:
         try:
             # Request only the one last commit for the supplied user's repository with supplied name.
             self.log.info('Inside retrieve_latest_commit_date_for_github_repository function')
-            # github_request_path = f"/repos/{str(self.owner)}/{in_repository_name}/commits?page=1&per_page=1"
-            github_request_path = f"/repos/{str(self.owner)}/{in_repository_name}/commits"
+            github_request_path = f"/repos/{str(self.owner)}/{in_repository_name}/commits?page=1&per_page=1"
+            # github_request_path = f"/repos/{str(self.owner)}/{in_repository_name}/commits"
             self.log.warning(f'github_request_path -> {github_request_path}')
             
             https_conn.request(url=github_request_path,method='GET',headers=http_request_headers)
@@ -114,30 +125,36 @@ class Automation:
                 curr_time = datetime.strptime(curr_time,"%Y-%m-%dT%H:%M:%SZ")
                 self.log.warning(f'Current Time -> {curr_time}')
                 
-                for i in range(len(api_resp_object)):
-                    last_commit_ref = api_resp_object[i]['sha']
-                    last_commit_msg = api_resp_object[i]['commit']['message']
-                    committed_by = api_resp_object[i]['commit']['committer']['name']
-                    repo_last_commit_date = api_resp_object[i]['commit']['author']['date']
-                    # self.log.warning(f'repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
-                    repo_last_commit_date = datetime.strptime(repo_last_commit_date, "%Y-%m-%dT%H:%M:%SZ")
-                    # self.log.warning(f'[CONVERT] repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
-                    
-                    
-                    interval = (curr_time - repo_last_commit_date)
-                    seconds = interval.total_seconds()
-                    hours = seconds // 3600
-                    minutes = (seconds % 3600) // 60
-                    seconds = seconds % 60
-                    
-                    self.log.info(f'Interval of Current Time & Last Commit Time :: {interval} Timedelta [ H :: {hours} | M :: {minutes} | S :: {seconds}]')
-                    
-                    if ((minutes > 0.0 and minutes < 5.0) and (hours == 0.0)):
-                        self.log.info('New Commit Found')
-                        latest_push = True
-                    else:
-                        self.log.info('No New Commit Found')
-                    self.log.warning(f'Reference {last_commit_ref} [{last_commit_ref[0:7]}] | Last Commit Message -> {last_commit_msg}\n::|:: BY - {committed_by} ON - {repo_last_commit_date}')                    
+                # for i in range(len(api_resp_object)):
+                #     last_commit_ref = api_resp_object[i]['sha']
+                #     last_commit_msg = api_resp_object[i]['commit']['message']
+                #     committed_by = api_resp_object[i]['commit']['committer']['name']
+                #     repo_last_commit_date = api_resp_object[i]['commit']['author']['date']
+                #     # self.log.warning(f'repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
+                #     repo_last_commit_date = datetime.strptime(repo_last_commit_date, "%Y-%m-%dT%H:%M:%SZ")
+                #     # self.log.warning(f'[CONVERT] repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
+                last_commit_ref = api_resp_object[0]['sha']
+                last_commit_msg = api_resp_object[0]['commit']['message']
+                committed_by = api_resp_object[0]['commit']['committer']['name']
+                repo_last_commit_date = api_resp_object[0]['commit']['author']['date']
+                # self.log.warning(f'repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
+                repo_last_commit_date = datetime.strptime(repo_last_commit_date, "%Y-%m-%dT%H:%M:%SZ")
+                # self.log.warning(f'[CONVERT] repository_last_commit_date -> {repo_last_commit_date} :: Datatype = {type(repo_last_commit_date)}')
+                
+                interval = (curr_time - repo_last_commit_date)
+                seconds = interval.total_seconds()
+                hours = seconds // 3600
+                minutes = (seconds % 3600) // 60
+                seconds = seconds % 60
+                
+                self.log.info(f'Interval of Current Time & Last Commit Time :: {interval} Timedelta [ H :: {hours} | M :: {minutes} | S :: {seconds}]')
+                
+                if ((minutes > 0.0 and minutes < 5.0) and (hours == 0.0)):
+                    self.log.info('New Commit Found')
+                    latest_push = True
+                else:
+                    self.log.info('No New Commit Found')
+                self.log.warning(f'Reference {last_commit_ref} [{last_commit_ref[0:7]}] | Last Commit Msg -> {last_commit_msg} ::|:: BY - {committed_by} ON - {repo_last_commit_date}')                    
             else:
                 message = f"ERROR: Request to GitHub failed with status {git_api_resp.status} and the reason was {git_api_resp.reason}"
                 self.log.error(f'Message -> {message}')
