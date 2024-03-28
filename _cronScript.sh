0cd4fec32ba5e3e59c2a43dcc3c87db20a7ed933
KinnarChowdhury1994 <kc1988@cse.jgec.ac.in>
2024-03-28 19:24:18 +0530

    cronjob modified

--- _cronScript.sh
+++ _cronScript.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
-cd /home/kinnar/deployment/automation
+cd /home/kinnar/deployment/
 ls -la
+cd .venv/bin && source activate
+cd ../.. && pwd && ls
+cd automation
 pip3 install -r requirements.txt
 python3 automate.py
+echo "Executed Successfully"
