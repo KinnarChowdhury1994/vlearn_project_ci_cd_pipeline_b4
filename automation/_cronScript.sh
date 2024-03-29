70fb3096a20b2e513791457f1ddd39dec23125fe
KinnarChowdhury1994 <kc1988@cse.jgec.ac.in>
2024-03-29 17:27:05 +0530

    modified document

--- automation/_cronScript.sh
+++ automation/_cronScript.sh
@@ -14,6 +14,8 @@ ls -la
 pip3 install -r requirements.txt
 pip3 list
 sudo chmod +x $HOME/repo/$REPO/automation/automate.py
+sudo chmod +x $HOME/repo/$REPO/automation/_deploy.sh
+
 echo "Executing automate.py from Shell"
 python3 $HOME/repo/$REPO/automation/automate.py
 echo "_cronScript Executed Successfully."
